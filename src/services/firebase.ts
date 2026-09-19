fffa0d719aa4af72b83675a998227bf3df78f08e
Victory Chibueze Akunna <victorychibuakunna@gmail.com>
2026-09-19 19:59:58 +0100

    added proper authentication

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -2,13 +2,13 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBVoL0p8Fnkjh3E5i1-9Yv0CHv3J7bymdY",
-  authDomain: "achivsecurities-65426.firebaseapp.com",
-  projectId: "achivsecurities-65426",
-  storageBucket: "achivsecurities-65426.firebasestorage.app",
-  messagingSenderId: "547793921506",
-  appId: "1:547793921506:web:7940b5cb7cb5025c7f05bb",
-  measurementId: "G-XT6CS5YR63"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
